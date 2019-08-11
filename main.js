f1a111db2e1b6159f6d7c39f0bd4f3fa3e0af5cf
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-08-11 18:00:31 -0400

    project completed

--- main.js
+++ main.js
@@ -2,43 +2,34 @@ function getCurrentDateAndTime() {
   return new Date().toLocaleString();
 }
 
-
 // Our new array of objects!
 // Try prepopulating with two more todos to get yourself started.
 // You absolutely can put the `priority` as a different value than a number. It could be set to a string of 'high' or 'low'.
 // It could be a property `highPriority` that is true or false.
-let todos = [
-  {
-    name: 'prepopulate more todos',
-    date: getCurrentDateAndTime(),
-    completed: false,
-    priority: 1,
-  },
-]
+let todos = [];
 
 // TODO: uncomment below once you have printList and printTodo working!
 // This way you'll have a pre-populated todo list to play with.
 
-// printList();
-
 // Instead of taking in various parameters, we're going to pass in a whole todo object here.
 // Remember that you can access that todo's properties with dot notation!
 function printTodo(todo, todoIndex) {
+  const ol = document.querySelector(".todo-list");
 
-  const ol = document.querySelector('.todo-list');
-  
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.id = todoIndex;
-  
-  const todoNameElement = document.createElement('p');
-  const todoDateElement = document.createElement('p');
+
+  const todoNameElement = document.createElement("p");
+  const todoDateElement = document.createElement("p");
 
   // Set the todo elements above to the appropriate properties from the todo that was passed in.
-
+  todoNameElement.innerText = todo.name;
+  todoDateElement.innerText = todo.date;
 
   // If the todo's priority property is high priority (2, or whatever you've set it to), then do some extra styling on the li here. Bold? Red? Bigger font? Up to you!
-
-
+  if (todo.priority === "2") {
+    li.style.color = "red";
+  }
 
   li.appendChild(todoNameElement);
   li.appendChild(todoDateElement);
@@ -59,28 +50,35 @@ function printList() {
   }
 }
 
-function addTodo() {
+function addTodo(todo) {
   // What should we take in and what should we push in?
+  todos.push(todo);
 }
 
-const addButton = document.querySelector('.add-todo');
+const addButton = document.querySelector(".add-todo");
 addButton.onclick = addToList;
 
 function addToList() {
-  const inputBox = document.querySelector('.todo-input');
+  const inputBox = document.querySelector(".todo-input");
   const todoName = inputBox.value;
   const todoDate = getCurrentDateAndTime();
-  const priorityInput = document.querySelector('.priority-pulldown');
+  const priorityInput = document.querySelector(".priority-pulldown");
 
   // Be careful when grabbing the `.value` of priorityInput above. Whatever value they choose in their pulldown will come in as a string!
 
   // Now make a todo object out of all the values above! (And `false` for `completed`!)
-  
+  const todo = {
+    name: todoName,
+    date: todoDate,
+    completed: false,
+    priority: priorityInput.value
+  };
+
   // Now pass that todo into your `addTodo` and `printTodo` functions. Pay attention to what `printTodo` takes in as a parameter!
-
-
+  addTodo(todo);
+  printTodo(todo, todos.indexOf(todo));
+  inputBox.value = "";
 }
-
 
 function handleMark(event) {
   // Get the li
@@ -93,18 +91,19 @@ function handleMark(event) {
   markComplete(index);
 }
 
-
 function markComplete(index) {
   // If it's complete in the data, set it to not complete, and vice versa!
+  todos[index].completed = !todos[index].completed;
 }
 
 function printMark(index) {
   const li = document.querySelector(`li:nth-child(${index + 1})`);
-  
-    // Set the dom styling appropriately here based on whether it's complete or not!
+
+  // Set the dom styling appropriately here based on whether it's complete or not!
+  li.classList.toggle("cross");
 }
 
-const deleteButton = document.querySelector('.delete-completed-todos');
+const deleteButton = document.querySelector(".delete-completed-todos");
 deleteButton.onclick = handleDelete;
 
 function handleDelete() {
@@ -112,16 +111,20 @@ function handleDelete() {
 
   // Only push into newTodos any todo that is NOT complete.
 
-  
+  todos.forEach(todo => {
+    if (todo.completed === false) {
+      newTodos.push(todo);
+    }
+  });
+
   todos = newTodos;
   clearList();
   printList();
 }
 
-
 function clearList() {
   // Grab the todo list ul and put it in a variable
-  const list = document.querySelector('.todo-list');
+  const list = document.querySelector(".todo-list");
 
   // Remove all children of that list.
   // My favorite way uses `.hasChildNodes()` and `.remove()` and `.firstChild`, but there are other ways if you wanna research them instead!
